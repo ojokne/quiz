588b45dbaecccafb69557b54d60619d6f28d5e56
ojokne <ojokne@gmail.com>
2022-12-05 11:33:09 +0300

    Added new styles
    Added border bottom to h1
    Increased button widths from w-64 to w-72

--- src/pages/Difficulty.jsx
+++ src/pages/Difficulty.jsx
@@ -21,35 +21,37 @@ function Difficulty() {
   });
   return (
     <div className="flex flex-col h-96 justify-center items-center mt-3">
-      <h1 className="font-bold text-2xl m-3 p-3 text-sky-800">
-        Choose a difficulty
-      </h1>
-      <div>
-        <button
-          className="p-2 m-2 bg-sky-800 text-slate-200 w-64 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
-          id={DIFFICULTY.EASY}
-          onClick={() => handleChangeDifficulty(DIFFICULTY.EASY)}
-        >
-          Easy
-        </button>
-      </div>
-      <div>
-        <button
-          className="p-2 m-2 bg-sky-800 text-slate-200 w-64 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
-          id={DIFFICULTY.MEDIUM}
-          onClick={() => handleChangeDifficulty(DIFFICULTY.MEDIUM)}
-        >
-          Medium
-        </button>
-      </div>
-      <div>
-        <button
-          className="p-2 m-2 bg-sky-800 text-slate-200 w-64 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
-          id={DIFFICULTY.HARD}
-          onClick={() => handleChangeDifficulty(DIFFICULTY.HARD)}
-        >
-          Hard
-        </button>
+      <div className="border-2 rounded-md p-1.5 my-2.5 lg:mx-2.5  shadow-md">
+        <h1 className="font-bold border-b-2 text-2xl m-3 p-3 text-sky-800">
+          Choose a difficulty
+        </h1>
+        <div>
+          <button
+            className="p-2 m-2 bg-sky-800 text-slate-200 w-72 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+            id={DIFFICULTY.EASY}
+            onClick={() => handleChangeDifficulty(DIFFICULTY.EASY)}
+          >
+            Easy
+          </button>
+        </div>
+        <div>
+          <button
+            className="p-2 m-2 bg-sky-800 text-slate-200 w-72 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+            id={DIFFICULTY.MEDIUM}
+            onClick={() => handleChangeDifficulty(DIFFICULTY.MEDIUM)}
+          >
+            Medium
+          </button>
+        </div>
+        <div>
+          <button
+            className="p-2 m-2 mb-6 bg-sky-800 text-slate-200 w-72 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+            id={DIFFICULTY.HARD}
+            onClick={() => handleChangeDifficulty(DIFFICULTY.HARD)}
+          >
+            Hard
+          </button>
+        </div>
       </div>
     </div>
   );
