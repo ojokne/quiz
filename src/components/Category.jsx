382a15ba3dbd584dc47e77d12e7142f4813b0196
ojokne <ojokne@gmail.com>
2022-12-05 07:48:13 +0300

    Removed empty class attribute

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -8,7 +8,7 @@ function Category({ category, dispatch }) {
     navigate("/difficulty");
   }
   return (
-    <div className="">
+    <div>
       <button
         className="p-2 m-2 bg-sky-800 text-slate-200 w-64 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
         id={category.id}
