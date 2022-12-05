6d1afcd3d3ca43f25ef17ef6a651610e82343910
ojokne <ojokne@gmail.com>
2022-12-05 11:37:00 +0300

    Added next. change category and change difficulty buttons
    Added functionality to proceed to next set of questions
    Added functionality to change category and difficulty

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,14 +1,34 @@
 import { useEffect, useState } from "react";
-import { useAnswers, useOptions } from "../context/ContextProvider";
+import {
+  useAnswerDispatch,
+  useAnswers,
+  useOptions,
+} from "../context/ContextProvider";
 import { useNavigate } from "react-router-dom";
 import Review from "../components/Review";
+import { ACTIONS } from "../store/constants";
 
 function Results() {
   const answers = useAnswers();
   const options = useOptions();
   const navigate = useNavigate();
+  const answerDispatch = useAnswerDispatch();
   const [correct, setCorrect] = useState();
 
+  function handleNext() {
+    answerDispatch({ type: ACTIONS.CLEAR_STATE });
+    navigate("/questions");
+  }
+  function handleDifficulty() {
+    answerDispatch({ type: ACTIONS.CLEAR_STATE });
+    navigate("/difficulty");
+  }
+
+  function handleCategory() {
+    answerDispatch({ type: ACTIONS.CLEAR_STATE });
+
+    navigate("/");
+  }
   useEffect(() => {
     let count = 0;
     if (options.category === 0) {
@@ -26,11 +46,36 @@ function Results() {
   }, []);
 
   return (
-    <div>
-      <p className="font-bold text-center m-2 p-2">You passed {correct}</p>
-      {answers.map((a) => {
-        return <Review answer={a} key={a.id} />;
-      })}
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <p className="font-bold text-center m-2 p-2 text-sky-800">
+          You passed {correct}
+        </p>
+        {answers.map((a) => {
+          return <Review answer={a} key={a.id} />;
+        })}
+      </div>
+      <div className="flex flex-wrap-reverse justify-center ">
+        <button
+          className="p-2 m-2 bg-sky-800 text-slate-200 w-full lg:w-64  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+          onClick={handleCategory}
+        >
+          Change Category
+        </button>
+        <button
+          className="p-2 m-2 bg-sky-800 text-slate-200 w-full lg:w-64  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+          onClick={handleDifficulty}
+        >
+          Change Difficulty
+        </button>
+
+        <button
+          className="p-2 m-2 bg-sky-800 text-slate-200 w-full lg:w-64  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
