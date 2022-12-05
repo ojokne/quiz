4b4459387034c0cacb4b262a45d78b75c309958f
ojokne <ojokne@gmail.com>
2022-12-05 07:48:53 +0300

    Changed button name
    Renamed finish button to submit button

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -81,7 +81,7 @@ function Questions({
           className="p-2 m-2 bg-sky-800 text-slate-200 w-64 lg:w-60  font-medium rounded-md hover:ring-4 hover:ring-offset-2 hover:scale-95"
           onClick={handleFinish}
         >
-          Finish
+          Submit
         </button>
       </div>
     </div>
