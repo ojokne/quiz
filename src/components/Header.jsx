355c09d5b52bbe7f340ab9f08a66db7799187a46
ojokne <ojokne@gmail.com>
2022-12-06 06:11:05 +0300

    Linked h1 to home
    Added a link to the h1 which redirects to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Header() {
   const style = { color: "white", fontSize: "1.5em" };
   return (
     <div className="bg-sky-800 shadow shadow-slate-500">
       <div className="max-w-4xl m-auto flex flex-row justify-between items-center">
-        <h1 className="text-slate-200 p2.5 m-2.5 text-2xl font-bold">Quiz</h1>
+        <h1 className="text-slate-200 p2.5 m-2.5 text-2xl font-bold">
+          <Link to="/">Quiz</Link>
+        </h1>
         <span className="border-none lg-hidden p2.5 m-2.5 hover:ring-4 hover:rounded hover:ring-offset-slate-200 hover:ring-offset-4">
           <FaBars style={style} />
         </span>
