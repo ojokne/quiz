de0acb8f6b6fa8b5314b0365a34952a1bc5fe3a9
ojokne <ojokne@gmail.com>
2022-12-06 07:54:36 +0300

    Included separate menu for small and large screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Menu from "./Menu";
 
 function Header() {
+  const [display, setDisplay] = useState(false);
+  const [mobileStyle, setMobileStyle] = useState("hidden");
+
   const style = { color: "white", fontSize: "1.5em" };
+  const direction = {
+    row: "flex-row",
+    col: "flex-col",
+  };
+
+  function toggleMenu() {
+    setDisplay(!display);
+    if (display) {
+      setMobileStyle("lg:hidden");
+    } else {
+      setMobileStyle("hidden");
+    }
+  }
   return (
-    <div className="bg-sky-800 shadow shadow-slate-500">
-      <div className="max-w-4xl m-auto flex flex-row justify-between items-center">
-        <h1 className="text-slate-200 p2.5 m-2.5 text-2xl font-bold">
+    <div className="bg-sky-800 shadow shadow-slate-500 text-slate-200">
+      <div className="max-w-4xl m-auto flex flex-row justify-between items-center ">
+        <h1 className="p2.5 m-2.5 text-2xl font-bold">
           <Link to="/">Quiz</Link>
         </h1>
-        <span className="border-none lg-hidden p2.5 m-2.5 hover:ring-4 hover:rounded hover:ring-offset-slate-200 hover:ring-offset-4">
-          <FaBars style={style} />
-        </span>
+        <div className="mx-2 px-2">
+          <span onClick={toggleMenu} className="lg:hidden p2.5 m-2.5">
+            <FaBars style={style} />
+          </span>
+        </div>
+        <div className="hidden lg:block">
+          <Menu style={direction.row} />
+        </div>
+      </div>
+      <div className={mobileStyle}>
+        <Menu style={direction.col} />
       </div>
     </div>
   );
