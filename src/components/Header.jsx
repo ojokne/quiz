6902e0d1c5fdb59e893249f3dfcc286fc4baed5e
ojokne <ojokne@gmail.com>
2022-12-06 05:07:52 +0300

    Style update
    changed hover ring color from sky-800 to slate-200

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="bg-sky-800 shadow shadow-slate-500">
       <div className="max-w-4xl m-auto flex flex-row justify-between items-center">
         <h1 className="text-slate-200 p2.5 m-2.5 text-2xl font-bold">Quiz</h1>
-        <span className="lg-hidden p2.5 m-2.5 hover:ring-2 hover:rounded hover:ring-offset-sky-800 hover:ring-offset-4">
+        <span className="border-none lg-hidden p2.5 m-2.5 hover:ring-4 hover:rounded hover:ring-offset-slate-200 hover:ring-offset-4">
           <FaBars style={style} />
         </span>
       </div>
